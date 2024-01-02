ac5f066745229b11f3b0af532a261bc693c58fcd
dhanshree <dhanshreewagh16598@gmail.com>
2024-01-02 15:28:21 +0530

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,7 +28,7 @@ const obj4 = {6: "a", 7: "b"}
 // const obj3 = {obj1, obj2}
 
 const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+//console.log(obj3);
 
 const users = [
     {
@@ -46,10 +46,36 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(object.keys(tinderUser));
-console.log(object.values(tinderUser));
-console.log(object.entries(tinderUser));
+// console.log(object.keys(tinderUser));
+// console.log(object.values(tinderUser));
+// console.log(object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+//console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "dhara"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "dhara"
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+ 
+]
