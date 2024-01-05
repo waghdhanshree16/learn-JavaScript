69d625baabafef52f1a52e18fe31e9695ab90828
dhanshree <dhanshreewagh16598@gmail.com>
2024-01-05 12:55:27 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,54 @@
+// var c = 300
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+   // console.log("INNER:", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "dhara"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //  console.log(website);
+
+    two()
+}
+
+one()
+
+if (true) {
+    const username = "dhara"
+    if (username === "dhara") {
+        const website = " youtube"
+        // console.log(username + website);
+
+    }
+
+    //console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++ interesting ++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
