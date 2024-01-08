dcef94d4b69df461a65313eec486ad33478239d0
dhanshree <dhanshreewagh16598@gmail.com>
2024-01-08 15:36:57 +0530

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+if ( temperature === 41 ) {
+
+    // console.log("less than 50")
+} else {
+    console.log("temperature is greater than 50")
+}
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===
+
+//  const score = 200
+
+//  if (score > 100) {
+//     var power = "fly"
+//     console.log('User power: ${power}');
+//  }
+
+//  console.log('User power: ${power}');
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+if (balance < 500) {
+   console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750")
+} else if (balance < 900) {
+    console.log("less than 750")
+} else {
+    console.log("less than 1200");
+}
+
+const userloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userloggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+     console.log("User logged in");
+}
