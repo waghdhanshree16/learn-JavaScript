7b3af42c997a2f2a968f0d3f31f27133ec96d72f
dhanshree <dhanshreewagh16598@gmail.com>
2024-03-06 16:52:00 +0530

    object_get_set.js

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,17 @@
+function User(email,password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this.email = value
+        }
+    })
+}
+
+const coffee = new User("coffee@coffee.com", "coffee")
+
+console.log(coffee.email);
