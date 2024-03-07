9c5002f31fe541782b40fbbe1863f5fd038a9474
dhanshree <dhanshreewagh16598@gmail.com>
2024-03-07 14:07:47 +0530

    object_get_set.js

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,17 @@
+const User = {
+    _email: 'd@dm.com',
+    _password: "xyz"
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
